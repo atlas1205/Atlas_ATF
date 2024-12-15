30edb3a0194a27b5e018cb6330b646833b5904b5
Wiktor Kocielski <withaust@gmail.com>
2024-12-15 22:11:33 +0300

    2.0.3

--- prototypes/recipes/turret-recipes.lua
+++ prototypes/recipes/turret-recipes.lua
@@ -2,38 +2,6 @@ data:extend(
 {
   {
     type = "recipe",
-    name = "ATF-tesla-turret",
-    enabled = false,
-	energy_required = 10,
-    ingredients =
-    {
-			{type = "item", name = "copper-cable",amount = 20},
-			{type = "item", name = "steel-plate",amount = 10},
-			{type = "item", name = "electronic-circuit",amount = 10},
-    },
-		results = {
-		{type = "item", name = "ATF-tesla-turret", amount = 1}
-		},
-		main_product = "ATF-tesla-turret",
-  },  
-  {
-    type = "recipe",
-    name = "ATF-tesla-turret-2",
-    enabled = false,
-	energy_required = 20,
-    ingredients =
-    {
-			{type = "item", name = "ATF-tesla-turret",amount = 1},
-			{type = "item", name = "processing-unit", amount = 5},
-			{type = "item", name = "ATF-supercap", amount = 4},
-    },
-		results = {
-		{type = "item", name = "ATF-tesla-turret-2", amount = 1}
-		},
-		main_product = "ATF-tesla-turret-2",
-  },  
-  {
-    type = "recipe",
     name = "ATF-swatt-turret",
     enabled = false,
 	energy_required = 10,
@@ -110,7 +78,7 @@ data:extend(
 			{type = "item", name = "ATF-grenade-turret",amount = 1}
     },
 		results = {
-		{type = "item", name = "ATF-grenade-turret", amount = 1}
+		{type = "item", name = "ATF-grenade-turret-2", amount = 1}
 		}
   }, 
   {
